1cb904ac8c21e3d4976e2b5487855a7df2e103fa
Rohit Yadav <114579310+rohityadav7054@users.noreply.github.com>
2024-02-23 13:44:59 +0530

    Update app.js allowed all origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(Express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true, // Ensure this is set to true
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type,Authorization',
